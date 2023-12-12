05aa148feba2f0e863509c5fd4419b592b94b71f
LucasSimoesPolvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2023-12-12 16:32:18 +0100

    Allows the js to do the HTML and be responsive if we add something in the product's array

--- selfServiceMachineInitial/selfServiceMachineInitial/scripts.js
+++ selfServiceMachineInitial/selfServiceMachineInitial/scripts.js
@@ -1,3 +1,7 @@
+let nameSpan;
+let priceSpan;
+let quantitySpan;
+
 let products = [
   {
     photo: "img/big-mac.png",
@@ -6,6 +10,7 @@ let products = [
     active: false,
     quantity: 0,
   },
+  
   {
     photo: "img/mc-chicken.png",
     name: "Mc Chicken",
@@ -63,15 +68,77 @@ let products = [
     quantity: 0,
   },
 ];
-
+const move = () => {
+  updateElements();
+};
+setInterval(move, 200);
 function substractProduct(){
-
+  products[0].quantity++;
+  // var productElement = button.closest('.product')
 }
 
 function addProduct(){
 
 }
 
-function getElements(name, price, ){
-  
+function defineElements(){
+  let section = document.getElementsByClassName('items')
+  let item = section[0];
+
+  products.forEach(v => {
+    let productItem = document.createElement("div");
+    productItem.className = "product";
+    item.appendChild(productItem);
+
+    let photoDiv = document.createElement("div");
+    photoDiv.className = "photo";
+    productItem.appendChild(photoDiv);
+
+    let photoImg = document.createElement("img");
+    photoImg.src = v.photo;
+    photoDiv.appendChild(photoImg);
+
+    let descriptionDiv = document.createElement("div");
+    descriptionDiv.className = "description";
+    productItem.appendChild(descriptionDiv);
+
+    nameSpan = document.createElement("span");
+    nameSpan.className = "name";
+    nameSpan.textContent = v.name
+    descriptionDiv.appendChild(nameSpan);
+
+    let priceSpan = document.createElement("span");
+    priceSpan.className = "price";
+    priceSpan.textContent = v.price
+    descriptionDiv.appendChild(priceSpan);
+
+    let quantityareaDiv = document.createElement("div");
+    quantityareaDiv.className = "quantity-area";
+    descriptionDiv.appendChild(quantityareaDiv);
+
+    let minusButton = document.createElement("button");
+    minusButton.onclick = "substractProduct()";
+    minusButton.textContent = "-"
+    quantityareaDiv.appendChild(minusButton);
+
+    let quantitySpan = document.createElement("span");
+    quantitySpan.className = "quantity";
+    quantitySpan.textContent = v.quantity;
+    quantityareaDiv.appendChild(quantitySpan);
+
+    let plusButton = document.createElement("button");
+    plusButton.onclick = "addProduct()";
+    plusButton.textContent = "+"
+    quantityareaDiv.appendChild(plusButton);
+});
+}
+
+function updateElements(){
+  nameSpan = document.getElementsByClassName("name");
+  priceSpan = document.getElementsByClassName("price");
+  quantitySpan = document.getElementsByClassName("quantity")
+  products.forEach(v => {
+    
+  })
+
 }
